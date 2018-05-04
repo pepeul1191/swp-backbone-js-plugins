a9549efc59148db75d411d6da1d11fefe8e31e76
Pepe Valdivia <jvaldivia@softweb.pe>
2018-05-04 16:27:44 -0500

    Autocompletar backboneJS con modelos

--- views/autocomplete.js
+++ views/autocomplete.js
@@ -1,21 +1,25 @@
 var AutocompleteView = Backbone.View.extend({
-	el: '#formUbicaciones',
+	el: "#formUbicaciones",
 	initialize: function(params){
     // inicializar variables
-    this.idTarget = params['id'];
-    this.idNombre = params['nombre'];
-    this.idLabelMensajeError = params['targetMensajeError'];
-    this.targetSugerencias = params['targetSugerencias'];
-    this.mensajeError = params['mensajeError'];
-    this.collection = params['collection'];
+    this.idTarget = params["id"];
+    this.idNombre = params["nombre"];
+    this.idLabelMensajeError = params["targetMensajeError"];
+    this.targetSugerencias = params["targetSugerencias"];
+    this.mensajeError = params["mensajeError"];
+    this.collection = params["collection"];
+		this.url = params["url"];
+		this.model = params["model"];
     // asignacion dinamica de eventos
     this.events = this.events || {};
-    this.events['keyup #' + this.idNombre] = 'buscarCooincidencias';
+    this.events["keyup #" + this.idNombre] = "buscarCooincidencias";
+		this.events["focusout #" + this.idNombre] = "limpiarSiVacio";
     this.delegateEvents();
 	},
   events: {
     // se está usando asignacion dinamica de eventos en el constructor
     "click .sugerencia": "sugerenciaClick",
+		"click #verModelo": "verModelo",
   },
   buscarCooincidencias: function(event) {
     var textoIngresado = $(event.target).val();
@@ -23,13 +27,13 @@ var AutocompleteView = Backbone.View.extend({
     var viewInstance = this;
     $.ajax({
       type: "GET",
-      url: BASE_URL + "distrito/buscar",
+      url: viewInstance.url,
       data: {nombre: textoIngresado, csrfmiddlewaretoken: CSRF},
       async: false,
       success: function(data){
         var distritos = JSON.parse(data);
         for(var i = 0; i < distritos.length; i++){
-          var distrito = new Distrito({id: distritos[i]['id'], nombre: distritos[i]['nombre']});
+          var distrito = new Distrito({id: distritos[i]["id"], nombre: distritos[i]["nombre"]});
           viewInstance.collection.add(distrito);
         }
         $("#" + viewInstance.idLabelMensajeError).html("");
@@ -37,8 +41,12 @@ var AutocompleteView = Backbone.View.extend({
           var lista = ""
           $("#" + viewInstance.targetSugerencias).empty();
           viewInstance.collection.each(function(distrito) {
-            var temp = "<li><label class='oculto sugerencia'>" + distrito.get("id") + "</label><label>" + distrito.get("nombre") + "</label></li>";
-            lista = lista + temp;
+						var node = document.createElement("li");
+						node.classList.add("sugerencia");
+						node.setAttribute("id", distrito.get("id"));
+						var textnode = document.createTextNode(distrito.get("nombre"));
+						node.appendChild(textnode);
+						document.getElementById(viewInstance.targetSugerencias).appendChild(node);
           });
           $("#" + viewInstance.targetSugerencias).removeClass("oculto");
           $("#" + viewInstance.targetSugerencias).append(lista);
@@ -58,8 +66,18 @@ var AutocompleteView = Backbone.View.extend({
       }
     });
   },
+	limpiarSiVacio: function(event){
+		//var textoIngresado = $(event.target).val();
+		// TODO
+	},
   sugerenciaClick: function(event){
-    var target = event.target;
-    console.log(target);
+    $("#" + this.idTarget).html(event.target.getAttribute("id"));
+		$("#" + this.idNombre).val(event.target.innerText);
+		$("#" + this.targetSugerencias).empty();
+		this.model = this.collection.get(event.target.getAttribute("id"));
+		this.collection.reset();
   },
+	verModelo: function(event){
+		console.log(this.model.toString());
+	},
 });
